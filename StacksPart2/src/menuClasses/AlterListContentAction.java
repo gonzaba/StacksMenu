b6ffd7f05fa9dd58c6a4add2f5cb1a1ad067eb58
Barbara P. Gonzalez-Rivera <barbara.gonzalez1@upr.edu>
2018-03-26 10:46:50 -0400

    Lab Done

---
+++ StacksPart2/src/menuClasses/AlterListContentAction.java
@@ -0,0 +1,16 @@
+package menuClasses;
+
+import dataManager.DMComponent;
+
+public class AlterListContentAction implements Action {
+
+	@Override
+	public void execute(Object args) {
+
+		DMComponent dm = (DMComponent) args; 
+		dm.getMenuStack().push(AlterListContentMenu.getAlterListContentMenu()); 
+	}
+
+}
+
+
